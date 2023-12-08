2a45fd58145c60871d7b6569075546463c72b85e
Josep Audenis Ros <josep.audenis@students.salle.url.edu>
2023-12-08 08:47:03 +0100

    day 8 hard

--- aoc-d8/src/Main.java
+++ aoc-d8/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         input = readFromFile();
         List<List<String>> nodes = new ArrayList<>();
         List<Character> instructions = new ArrayList<>();
-        int count = 0;
+        long count = 0;
         for (char ch : input.get(0).toCharArray()){
             instructions.add(ch);
         }
@@ -28,50 +28,54 @@ public class Main {
             nodes.add(node);
         }
         int j = 0;
+        List<Long> steps = new ArrayList<>();
         List<String> actualNode = new ArrayList<>();
         for (List<String> node : nodes){
-            if (node.getFirst().equals("AAA")){
+            if (node.getFirst().charAt(2) == 'A') {
                 actualNode = node;
-                break;
-            }
-        }
-        while (true){
-            for (char instruction : instructions){
-                System.out.println(instruction + " - " + j + " - " + count + " - " + actualNode);
-                j++;
-                for (int i = 0; i < nodes.size(); i++){
-                    List<String> comparingNode = nodes.get(i);
-                    if (actualNode.getFirst().equals("ZZZ")){
+                count = 0;
+                while (true) {
+                    for (char instruction : instructions) {
+                        //System.out.println(instruction + " - " + j + " - " + count + " - " + actualNode);
+                        j++;
+                        for (int i = 0; i < nodes.size(); i++) {
+                            List<String> comparingNode = nodes.get(i);
+                            if (actualNode.getFirst().charAt(2) == 'Z') {
+                                break;
+                            }
+                            //System.out.println(actualNode  + "?=" + comparingNode.getFirst());
+                            if (instruction == 'L') {
+                                if (actualNode.get(1).equals(comparingNode.getFirst())) {
+                                    actualNode = comparingNode;
+                                    count++;
+                                    break;
+                                }
+                                if (i == nodes.size() - 1) i = -1;
+                                continue;
+                            }
+                            if (instruction == 'R') {
+                                if (actualNode.getLast().equals(comparingNode.getFirst())) {
+                                    actualNode = comparingNode;
+                                    count++;
+                                    break;
+                                }
+                                if (i == nodes.size() - 1) i = 0;
+                                continue;
+                            }
+                        }
+                    }
+                    if (actualNode.getFirst().charAt(2) == 'Z') {
                         break;
                     }
-                    //System.out.println(actualNode  + "?=" + comparingNode.getFirst());
-                    if (instruction == 'L') {
-                        if (actualNode.get(1).equals(comparingNode.getFirst())) {
-                            actualNode = comparingNode;
-                            count++;
-                            break;
-                        }
-                        if (i == nodes.size()-1) i = -1;
-                        continue;
-                    }
-                    if (instruction == 'R') {
-                        if (actualNode.getLast().equals(comparingNode.getFirst())) {
-                            actualNode = comparingNode;
-                            count++;
-                            break;
-                        }
-                        if (i == nodes.size()-1) i = 0;
-                        continue;
-                    }
                 }
-            }
-            if (actualNode.getFirst().equals("ZZZ")){
-                break;
+                steps.add(count);
             }
         }
-        System.out.println(count);
-
-
+        long result = steps.get(0);
+        for (int i = 0; i < steps.size(); i++){
+            result = lcm(result, steps.get(i));
+        }
+        System.out.println(result);
     }
 
     public static List<String> readFromFile(){
@@ -81,5 +85,14 @@ public class Main {
             System.out.println("Can't open file :(");
             return null;
         }
+    }
+
+    public static long gcd(long a, long b){
+        if (a == 0) return b;
+        return gcd(b%a,a);
+    }
+
+    public static long lcm(long a, long b){
+        return (a / gcd(a,b)) *b;
     }
 }
